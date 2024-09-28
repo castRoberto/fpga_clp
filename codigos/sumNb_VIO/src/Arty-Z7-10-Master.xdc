69c717bf51cf3b5b04bf140380c8c3bea5ac3de0
Roberto <robertoca07@hotmail.com>
2024-09-28 13:11:25 +0200

    Se implementa primer proyecto en una fpga

--- codigos/sumNb_VIO/src/Arty-Z7-10-Master.xdc
+++ codigos/sumNb_VIO/src/Arty-Z7-10-Master.xdc
@@ -4,7 +4,7 @@
 ## - rename the used ports (in each line, after get_ports) according to the top level signal names in the project
 
 ## Clock Signal
-#set_property -dict { PACKAGE_PIN H16    IOSTANDARD LVCMOS33 } [get_ports { clk }]; #IO_L13P_T2_MRCC_35 Sch=SYSCLK
+set_property -dict { PACKAGE_PIN H16    IOSTANDARD LVCMOS33 } [get_ports { clk_i }]; #IO_L13P_T2_MRCC_35 Sch=SYSCLK
 #create_clock -add -name sys_clk_pin -period 8.00 -waveform {0 4} [get_ports { clk }];#set
 
 ## Switches
--- codigos/sumNb_VIO/src/sumNb_VIO.vhd
+++ codigos/sumNb_VIO/src/sumNb_VIO.vhd
@@ -13,13 +13,24 @@ end;
 
 architecture sumNb_VIO_arq of sumNb_VIO is
 	-- Seccion declarativa
+	
+	COMPONENT vio_0
+      PORT (
+        clk : IN STD_LOGIC;
+        probe_in0 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+        probe_in1 : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
+        probe_out0 : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+        probe_out1 : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+        probe_out2 : OUT STD_LOGIC_VECTOR(0 DOWNTO 0)
+      );
+    END COMPONENT;
 
-	probe_a: std_logic_vector(3 downto 0);
-	probe_b: std_logic_vector(3 downto 0);
-	probe_s: std_logic_vector(3 downto 0);
+	signal probe_a: std_logic_vector(3 downto 0);
+	signal probe_b: std_logic_vector(3 downto 0);
+	signal probe_s: std_logic_vector(3 downto 0);
 
-	probe_ci: std_logic_vector(0 downto 0);
-	probe_co: std_logic_vector(0 downto 0);
+	signal probe_ci: std_logic_vector(0 downto 0);
+	signal probe_co: std_logic_vector(0 downto 0);
 
 begin
 
@@ -36,5 +47,15 @@ begin
 			s_o => probe_s,
 			co_o => probe_co(0)
 		);
+		
+		VIO_i : vio_0
+          PORT MAP (
+            clk => clk_i,
+            probe_in0 => probe_s,
+            probe_in1 => probe_co,
+            probe_out0 => probe_a,
+            probe_out1 => probe_b,
+            probe_out2 => probe_ci
+          );
 	
 end;
